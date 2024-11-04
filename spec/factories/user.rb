b78755ccf149c0abdb32cf3dc1165ba596e0aed4
Saul Zamora <szamora@pernix-solutions.com>
2024-11-03 21:39:38 -0600

    fix tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,5 +3,13 @@ FactoryBot.define do
     factory(:user) do
       email { Faker::Internet.email }
       password { Faker::Internet.password }
+
+      trait(:full_access) do
+        after(:create) do |user|
+          role = create(:role, :full_access)
+          user.roles << role
+          user.save!
+        end
+      end
     end
   end
